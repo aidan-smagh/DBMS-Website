54f43d687a76aed5c61be7d124f0260c6b84ebc4
Aidan <asmagh@cpsc.umw.edu>
2024-10-24 15:13:36 -0400

    Setting up the table on the homepage

--- routes.py
+++ routes.py
@@ -1,9 +1,15 @@
 from dbtour import dbtour_app
 import sqlite3
+from flask import render_template
 
 @dbtour_app.route("/")
 def does_this_work():
     conn = sqlite3.connect("asmagh_games.db")
-    cursor = conn.execute("SELECT * FROM games")
-    games = cursor.fetchall()
-    return f"<HTML><BODY><H1>{games[0][0]}</H1></BODY></HTML>"
+    
+    cursor = conn.execute("SELECT title FROM games")
+    titles = cursor.fetchall()
+
+    cursor = conn.execute("SELECT rating FROM games")
+    ratings = cursor.fetchall()
+
+    return render_template("home.html", titles = titles, ratings = ratings) 
